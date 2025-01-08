e750259a1449e5b64469c6e945e952e463644083
Jaddaoui Ayoub <Jaddaoui.salim@gmail.com>
2025-01-08 14:44:19 +0100

    changement url de l'url de cmi

--- src/main/java/com/example/service_utilisateur/client/CmiClient.java
+++ src/main/java/com/example/service_utilisateur/client/CmiClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(name = "cmi", url = "http://localhost:8089/api/cmi")
+@FeignClient(name = "cmi", url = "https://cmiservice-production.up.railway.app/api/cmi")
 
 public interface CmiClient {
     @PutMapping("/{compteId}/{utilisateurId}/assigner-utilisateur")

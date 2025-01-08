7188291aa4249083968f980c5b28cf877fa0f000
Jaddaoui Ayoub <Jaddaoui.salim@gmail.com>
2025-01-08 23:23:28 +0100

    cross

--- src/main/java/com/example/service_utilisateur/controller/AuthenticationController.java
+++ src/main/java/com/example/service_utilisateur/controller/AuthenticationController.java
@@ -8,14 +8,12 @@ import com.example.service_utilisateur.model.User;
 import com.example.service_utilisateur.service.AuthenticationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/auth")
 @RestController
 @AllArgsConstructor
+@CrossOrigin("*")
 public class AuthenticationController {
 //    private final JwtService jwtService;
     private final AuthenticationService authenticationService;
--- src/main/java/com/example/service_utilisateur/controller/UserController.java
+++ src/main/java/com/example/service_utilisateur/controller/UserController.java
@@ -3,13 +3,12 @@ package com.example.service_utilisateur.controller;
 import com.example.service_utilisateur.dto.UserDto;
 import com.example.service_utilisateur.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin("*")
+
 public class UserController {
     @Autowired
     private UserService userService;
